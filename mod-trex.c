423290d66da820131857ae002e4e141f96f0f70d
Matt Valentine-House <matt@eightbitraptor.com>
2016-07-08 16:26:39 +0100

    Follow module guidelines better
    define a version and description at the top of the file.
    
    Use the __init and __exit macros to define my module entry and exit points

--- mod-trex.c
+++ mod-trex.c
@@ -1,7 +1,9 @@
-#define __KERNEL__
-#define MODULE
-
 #include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("eightbitraptor");
+MODULE_DESCRIPTION("A device that roars at you");
+MODULE_VERSION("0.1");
 
 int things(void)
 {
@@ -9,17 +11,17 @@ int things(void)
         return 0;
 }
 
-int init_module()
+static int __init trex_init(void)
 {
         printk("Hello, world - this is the kernel speaking\n");
         things();
         return 0;
 }
 
-void cleanup_module()
+static void __exit trex_exit(void)
 {
         printk("Short is the life of an LKM\n");
 }
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("eightbitraptor");
+module_init(trex_init);
+module_exit(trex_exit);

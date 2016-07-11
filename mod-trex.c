e3778cc3aed3a8d0aca6a7f540cef42baf753c4d
Matt Valentine-House <matt@eightbitraptor.com>
2016-07-11 09:48:51 +0100

    Create the /dev/trex character device.
    This passes the currently failing bats test for the presence of
    /dev/trex
    
    This commit creates a character device in the device node tree. This
    device currently does nothing. Actually, not strictly true, this device
    currently crashes. Hard.
    
    This commit does a couple of things. Firstly it defines some meta data
    for the device that it's creating:
    
    DEVICE_NAME: This will be the point in the /dev/ tree that gets created
    DEVICE_CLASS: This creates a custom class for the device. I don't know
                  exactly what this is yet, but from what I can see it's
                  used to segregate different devices in sysfs. So with the
                  custom class trex. I can see my device in /sys/class/trex/
    
    Then it defines the interface for the device. The device is a character
    device so we need to map some file operations for when the device is
    opened for the first time, read from, written to and closed. This should
    do us for now.
    
    The device pointer is a struct that's defined within the kernel, and we
    map the device instance the it's struct of file operations together
    wihin the `register_chrdev` function call.
    
    `register_chrdev` attempts to register a device with the kernel (and
    assign a Major number for this driver, which is returned - these are the
    numbers defined in /proc/devices, each major number represents a driver,
    all devices with the same major number are controlled by the same
    driver).
    
    so the order of execution in the module init code is:
    
    * register the driver (get a new Major number)
    * create a char class
    * create a new device node in that char class
    
    Currently there is no code to tear all this down in the module exit
    hook, so all this cruft will hang around until the machine is rebooted.
    
    Also I have no idea how to go about finding an existing class and
    registering a new device in that class, or even if that is a good idea.

--- mod-trex.c
+++ mod-trex.c
@@ -1,20 +1,66 @@
 #include <linux/module.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+
+#define DEVICE_NAME "trex"
+#define CLASS_NAME "trex"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("eightbitraptor");
 MODULE_DESCRIPTION("A device that roars at you");
 MODULE_VERSION("0.1");
 
-int things(void)
-{
-        printk(KERN_CRIT "I'm in ur kernel");
+static int majorNumber;
+static struct class*  charClass  = NULL;
+static struct device* charDevice = NULL;
+
+static int dev_open(struct inode *inodep, struct file *filep) {
         return 0;
 }
 
+static int dev_release(struct inode *inodep, struct file *filep) {
+        return 0;
+}
+
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset){
+        return 0;
+}
+
+static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset) {
+        return 0;
+}
+
+static struct file_operations fops =
+{
+        .open = dev_open,
+        .read = dev_read,
+        .write = dev_write,
+        .release = dev_release,
+};
+
 static int __init trex_init(void)
 {
-        printk("Hello, world - this is the kernel speaking\n");
-        things();
+        majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
+        if (majorNumber<0){
+                printk(KERN_ALERT "failed to register a major number\n");
+                return majorNumber;
+        }
+
+        charClass = class_create(THIS_MODULE, CLASS_NAME);
+        if (IS_ERR(charClass)){
+                unregister_chrdev(majorNumber, DEVICE_NAME);
+                printk(KERN_ALERT "Failed to register device class\n");
+                return PTR_ERR(charClass);
+        }
+
+        charDevice = device_create(charClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
+        if (IS_ERR(charDevice)){
+                class_destroy(charClass);
+                unregister_chrdev(majorNumber, DEVICE_NAME);
+                printk(KERN_ALERT "Failed to create the device\n");
+                return PTR_ERR(charDevice);
+        }
+        printk(KERN_INFO "device class created correctly\n");
         return 0;
 }
 
